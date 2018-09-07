c657e910e0408d362dd19344a264f66dd3d18a9e
Zhi Ming Xu <zhi@rossini.mrc-bsu.cam.ac.uk>
2018-09-07 13:25:28 +0100

    test updates

--- Blood_Pressure_Small_Data_Test/run_pairwise_interaction_test.sh
+++ Blood_Pressure_Small_Data_Test/run_pairwise_interaction_test.sh
@@ -1,1 +1,11 @@
-plink --file ${1} --epistasis --allow-no-sex --epi1 1
+FILE_PREFIX=${1}
+TARGET_SNP=${2}
+#Target gene which mimics a drug
+rm -f $FILE_PREFIX".set"
+echo -e "TARGET_SNP\n"$TARGET_SNP"\nEND\n" >> $FILE_PREFIX".set"
+#All other SNPs
+echo -e "OTHER_SNPS" >> $FILE_PREFIX".set"
+awk '$2!=$TARGET_SNP {print$2}' $FILE_PREFIX".bim" >> $FILE_PREFIX".set"
+echo -e "END" >> $FILE_PREFIX".set"
+#Run PLINK test for interaction
+plink --file $FILE_PREFIX  --epistasis --allow-no-sex --epi1 1 --set-test --set $FILE_PREFIX".set"
