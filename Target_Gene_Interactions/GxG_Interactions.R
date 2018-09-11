275f2b2640232586f3a87d321222497ad7df494c
Zhi Ming Xu <zhi@rossini.mrc-bsu.cam.ac.uk>
2018-09-11 17:46:24 +0100

    GxG interactions

--- Target_Gene_Interactions/GxG_Interactions.R
+++ Target_Gene_Interactions/GxG_Interactions.R
@@ -1,31 +1,71 @@
-phenotype = 'sbp'
-targetRS <- 'rs603424'
-
-library(dplyr)
-source('LoadBgen.R')
-#Calculate allele dosage based on genotype probability
-alleleProbMatrix <- genotype_data$data
-remove(genotype_data)
-dosageMatrix <- matrix(0,nrow = nrow(alleleProbMatrix),ncol = ncol(alleleProbMatrix)) + alleleProbMatrix[,,'g=1'] + 2*alleleProbMatrix[,,'g=2']
-remove(alleleProbMatrix)
-
-#Construct Sample vs Phenotype Table
-samplePhenoTbl <- dplyr::select(samplesTbl,'samples'='ID_1',phenotype)
-remove(samplesTbl)
-colnames(dosageMatrix) <- samplePhenoTbl$samples
-
-#Function which calculates significance of interaction between two SNPs
-CalcInteractions <- function(dosageSubMatrix,phenotypes){
-  #Merge into data frame
-  data <- data.frame(SNP1=dosageSubMatrix[1,],SNP2=dosageSubMatrix[2,],Pheno=as.numeric(phenotypes))
-  #Linear model, with GxG interaction plus individual main effect
-  fit <- lm('Pheno~SNP1*SNP2',data = data)
-  #Return significance of interaction term
-  return(summary(fit)$coefficients[4,4])
+RunGxGInteractions <- function(path,sample_file_prefix,bgen_file_prefix,phenotype,targetRS){
+  chunkSize=100000
+  
+  library(dplyr)
+  library(parallel)
+  library(data.table)
+  source('LoadBgen.R')
+  
+  #Find all rsids, and generate chunks to read.
+  allRSIds <- FindAllRSIds(path,bgen_file_prefix) %>% dplyr::filter(rsid!=targetRS)
+  rsIDChunks <- split(allRSIds,seq(nrow(allRSIds)-1)%/%chunkSize)
+  samplesTbl <- LoadSamples(path,sample_file_prefix)
+  
+  #Intialize results vector
+  resultsTbl <- data.frame('rsid'=rep('',nrow(allRSIds)),'p' = rep(NA,nrow(allRSIds)), 'coeff'=rep(NA,nrow(allRSIds)),stringsAsFactors = F)
+  
+  for(i in 1:length(rsIDChunks)){
+    currentRSIdChunk <- rsIDChunks[[i]]
+    genotype_data <- LoadBgen(path,bgen_file_prefix,currentRSIdChunk,targetRS)
+    
+    #Calculate allele dosage based on genotype probability
+    alleleProbMatrix <- genotype_data$data
+    remove(genotype_data)
+    dosageMatrix <- matrix(0,nrow = nrow(alleleProbMatrix),ncol = ncol(alleleProbMatrix)) + alleleProbMatrix[,,'g=1'] + 2*alleleProbMatrix[,,'g=2']
+    remove(alleleProbMatrix)
+    
+    #Construct Sample vs Phenotype Table
+    samplePhenoTbl <- dplyr::select(samplesTbl,'samples'='ID_1',phenotype)
+    colnames(dosageMatrix) <- samplePhenoTbl$samples
+    
+    #Function which calculates significance of interaction between two SNPs
+    CalcInteractions <- function(dosageSubMatrix,phenotypes){
+      #Merge into data frame
+      data <- data.frame(SNP1=dosageSubMatrix[1,],SNP2=dosageSubMatrix[2,],Pheno=as.numeric(phenotypes))
+      #Linear model, with GxG interaction plus individual main effect
+      fit <- lm('Pheno~SNP1*SNP2',data = data)
+      #Return significance of interaction term
+      return(summary(fit)$coefficients[4,c(4,1)])
+    }
+    
+    #save results
+    nonTargetSnps <- setdiff(rownames(dosageMatrix),targetRS)
+    for(k in 1:length(nonTargetSnps)){
+      resultsTbl[((i-1)*chunkSize+k),1] <- nonTargetSnps[k]
+      resultsTbl[((i-1)*chunkSize+k),c(2,3)] <- CalcInteractions(dosageMatrix[c(nonTargetSnps[k],targetRS),],samplePhenoTbl[,phenotype])
+    }
+  }
+  data.table::fwrite(resultsTbl,file = paste0('~/',targetRS,'_',phenotype,'/',bgen_file_prefix,'.interactions.txt'),sep = '\t',row.names = F)
 }
+##First read in the arguments listed at the command line
+args=(commandArgs(TRUE))
 
-nonTargetSnps <- 'rs12572586' #setdiff(rownames(dosageMatrix),targetRS)
-interactionWithTarget <- mclapply(nonTargetSnps,
-                                function(x) CalcInteractions(rbind(dosageMatrix[x,],
-                                                                   dosageMatrix[targetRS,]),
-                                                             samplePhenoTbl[,phenotype]),mc.cores=20)
+##args is now a list of character vectors
+## First check to see if arguments are passed.
+## Then cycle through each element of the list and evaluate the expressions.
+if(length(args)==0){
+  print("No arguments supplied.")
+  ##supply default values
+  path <- '~/blood_pressure_test/'
+  sample_file_prefix <- 'ukbb_eur_all_sbp'
+  bgen_file_prefix <- 'sbp_chr10'
+  phenotype = 'sbp'
+  targetRS <- 'rs603424'
+  
+}else{
+  for(i in 1:length(args)){
+    eval(parse(text=args[[i]]))
+  }
+}
+RunGxGInteractions(path,sample_file_prefix,bgen_file_prefix,phenotype,targetRS)
+  
--- Target_Gene_Interactions/LoadBgen.R
+++ Target_Gene_Interactions/LoadBgen.R
@@ -1,11 +1,27 @@
-sample_file_prefix <- 'ukbb_eur_all_sbp'
-bgen_file_prefix <- 'sbp_chr10'
-library(rbgen)
-#Load sample file
-samplesTbl <- read.table(file = paste0(sample_file_prefix,'.sample'),header = F,stringsAsFactors = F)
-colnames(samplesTbl) <- samplesTbl[1,]#Add header as first row
-samplesTbl <- samplesTbl[-c(1,2),]#Remove first two rows
-
-#Load bgen file
-genotype_data <- rbgen::bgen.load(filename = paste0(bgen_file_prefix,'.bgen'),
-                                  ranges = data.frame(chromosome=10,start=1,end=1.35e8))
+LoadBgen <- function(path,bgen_file_prefix,rsIDs,targetRS){
+  library(rbgen)
+  #Load bgen file
+  curwd <- getwd()
+  setwd(path)
+  genotype_data <- rbgen::bgen.load(filename = paste0(bgen_file_prefix,'.bgen'),
+                                    rsids = c(as.character(rsIDs$rsid),targetRS))
+  setwd(curwd)
+  return(genotype_data)
+}
+LoadSamples <- function(path,sample_file_prefix){
+  #Load sample file
+  samplesTbl <- read.table(file = paste0(path,sample_file_prefix,'.sample'),header = F,
+                           stringsAsFactors = F)
+  colnames(samplesTbl) <- samplesTbl[1,]#Add header as first row
+  samplesTbl <- samplesTbl[-c(1,2),]#Remove first two rows
+  return(samplesTbl)
+}
+FindAllRSIds <- function(path,bgen_file_prefix){
+  library(dplyr)
+  bgenSummary <- system(paste0('bgenix -g ',path,bgen_file_prefix,'.bgen',' -list'),intern = T)
+  #Remove comment headers
+  bgenSummary <- bgenSummary[-c(1,length(bgenSummary))]
+  bgenSummary <- read.delim(text = bgenSummary,header = T)
+  rsIDs <- dplyr::select(bgenSummary,rsid,chromosome)
+  return(rsIDs)
+}
